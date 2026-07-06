30b2be420f9376a1b94775b6206084ec4ba7cfd2
Anita <akainth058@gmail.com>
2026-07-06 17:08:48 +0530

    fix build script

--- server.js
+++ server.js
@@ -9,16 +9,27 @@ dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 
+// Optional home route
+app.get("/", (req, res) => {
+  res.send("Auth API is running 🚀");
+});
+
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Auth API running on port 5000");
+// Render uses process.env.PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
